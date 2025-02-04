8af1d089f2dc3a1d7225e5cf11f7d3b02362e379
Aleksandra Niedźwiecka <olaniedz00@wp.pl>
2025-02-04 18:22:21 +0100

    SignUp view

---
+++ src/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿namespace CosmeticSalon.WebApi.Controllers;
+
+using CosmeticSalon.Application.Commands;
+using CosmeticSalon.WebApi.Models;
+
+[ApiController]
+public sealed class UsersController : ApiController
+{
+    [Route("SignUp")]
+    public IActionResult SignUpView()
+    {
+        return this.View();
+    }
+
+    [HttpPost, Route("SignUp")]
+    public async Task<IActionResult> SignUpView([FromForm] SignUpUser user)
+    {
+        if (this.ModelState.IsValid is false)
+        {
+            return this.View(user);
+        }
+
+        var id = Guid.NewGuid();
+
+        var command = new SignUp
+        {
+            Email = user.Email,
+            Password = user.Password,
+            UserId = id,
+            Username = user.Username,
+        };
+
+        await this.Mediator.Send(command);
+
+        return this.RedirectToPage("Index");
+    }
+}
---
+++ src/WebApi/Models/SignUpUser.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmeticSalon.WebApi.Models;
+
+public sealed class SignUpUser
+{
+    public string Email { get; init; }
+    public Guid Id { get; init; }
+    public string Password { get; init; }
+    public string ReapedPassword { get; init; }
+    public string Username { get; init; }
+}
